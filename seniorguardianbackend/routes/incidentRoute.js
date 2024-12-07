4e258ca4a237c1c623a646787363d48fb62e2110
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2024-12-07 18:33:45 +0530

    Delete

--- seniorguardianbackend/routes/incidentRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const multer = require('../middleware/multer'); // Import multer configuration
-const { reportIncident } = require('../controllers/incidentController');
-
-const router = express.Router();
-
-router.post('/report-incident', multer.single('file'), reportIncident); // Use multer for file uploads
-
-module.exports = router;
